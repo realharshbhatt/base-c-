9ae3b2ed36cd3e6c2ade2fd2623ff6e7d6cca7cd
harsh bhatt <35441945+realharshbhatt@users.noreply.github.com>
2019-09-11 23:12:34 +0530

    Add files via upload

---
+++ compair 2 integers.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"enter first value, a="; cin>>a;
+    cout<<"enter second value, b="; cin>>b;
+    if(a>b)
+    {
+        cout<<"a"<<a<<" is greater.";
+    }
+    else
+    {
+        cout<<"b"<<b<<" is greater.",b;
+    }
+    
+}
